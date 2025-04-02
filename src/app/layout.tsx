decf777c17372ec4dc074a5247ca8f8114062477
Ahmad <3ahmadnaif@gmail.com>
2025-04-02 15:58:17 +0300

    new logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,14 +10,14 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | The Startup View",
-    default: "The Startup View",
+    template: "%s | ستارت اب فيو",
+    default: "ستارت اب فيو",
   },
-  description: "Your gateway to startup insights and services",
+  description: "بوابتك للأستكشاف في عالم الشركات الناشئة وصناديق الأستثمار",
   viewport: "width=device-width, initial-scale=1",
   themeColor: "#ffffff",
   icons: {
-    icon: "/favicon.ico",
+    icon: "startupviewfavicon.png",
   },
 };
 
