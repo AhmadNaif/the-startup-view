387e95f8ba2cd08607a9f377ebd3a400e1458069
Ahmad <3ahmadnaif@gmail.com>
2025-04-02 15:05:30 +0300

    vercel deployment failes fixed

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["raed.vc"],
-  },
-};
-
-export default nextConfig;
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,13 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbdCCUsU2c8JV_8IGsXoRDHvHkR_uXgjE",
-  authDomain: "thestartupview.firebaseapp.com",
-  projectId: "thestartupview",
-  storageBucket: "thestartupview.firebasestorage.app",
-  messagingSenderId: "1089441557632",
-  appId: "1:1089441557632:web:f9e3ab1ef5cd4cbf65ca65",
-  measurementId: "G-RP802LP035",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
